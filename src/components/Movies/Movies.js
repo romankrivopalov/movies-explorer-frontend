8f2fb32f50e8c4e7f36e9328be35a479c2df3cf9
laptop <the.red.lemon@yandex.ru>
2023-06-17 17:11:01 +0300

    create Movie component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,15 @@
-import Header from "../Header/Header.js";
+import Header from '../Header/Header.js';
+import SearchForm from './SearchForm/SearchForm.js';
+
+import Footer from '../Footer/Footer.js'
 
 function Movies() {
   return(
     <div className="layout">
       <Header/>
+      <SearchForm/>
+
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -0,0 +1,30 @@
+function SearchForm() {
+  return(
+    <form className="search-form">
+      <label className="search-form__wrapper">
+        <input
+          type="text"
+          placeholder="Фильм"
+          className="search-form__input"/>
+        <button className="search-form__submit-btn">
+          Найти
+        </button>
+      </label>
+      <label className="search-form__wrapper search-form__wrapper_short-film">
+        <p className="search-form__short-film-text">
+          Короткометражки
+        </p>
+        <input
+          type="checkbox"
+          name="short-film-toggle"
+          id="short-film-toggle"
+          className="search-form__checkbox"/>
+        <label
+          className="search-form__checkbox-label"
+          for="short-film-toggle"/>
+      </label>
+    </form>
+  )
+}
+
+export default SearchForm

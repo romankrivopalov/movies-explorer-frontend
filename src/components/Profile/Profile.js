757adb43a119f110215fd9b3e0e1917ed75db938
laptop <the.red.lemon@yandex.ru>
2023-06-19 16:45:07 +0300

    fix styles and add handles

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,25 @@
 import { useContext } from 'react';
 import { CurrentUserContext } from '../../context/CurrentUserContext.js';
 import { Link } from 'react-router-dom';
-import Header from "../Header/Header";
+import Header from '../Header/Header';
 
 function Profile() {
   const { name, email } = useContext(CurrentUserContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(1);
+  }
+
+  function handleChange() {
+    console.log(2);
+  }
+
+  function handleLogout() {
+    console.log(3);
+  }
+
   return (
     <div className="layout layout_full-heigth">
       <Header
@@ -14,42 +29,52 @@ function Profile() {
         <h2 className="profile__title">
           {`Привет, ${name}!`}
         </h2>
-        <form className='profile__form'>
-          <label className='profile__input-container'>
-            <span className='profile__input-label'>
+        <form
+          id="profile__form"
+          className="profile__form"
+          onSubmit={handleSubmit}>
+          <label className="profile__input-container">
+            <span className="profile__input-label">
               Имя
             </span>
             <input
-              type='text'
-              name='profile-input-name'
-              id='profile-input-name'
-              className='profile__input'
-              placeholder='Имя'
-              value={name} // для редактирования нужен будет onChange
+              type="text"
+              name="profile-input-name"
+              id="profile-input-name"
+              className="profile__input"
+              placeholder="Имя"
+              value={name}
+              onChange={handleChange}
               minLength={2}
               maxLength={30}
               required={true}/>
           </label>
-          <span className='profile__divider'/>
-          <label className='profile__input-container'>
-            <span className='profile__input-label'>
+          <span className="profile__divider"/>
+          <label className="profile__input-container">
+            <span className="profile__input-label">
               E-mail
             </span>
             <input
-              type='email'
-              name='profile-input-name'
-              id='profile-input-name'
-              className='profile__input'
-              placeholder='Имя'
+              type="email"
+              name="profile-input-name"
+              id="profile-input-name"
+              className="profile__input"
+              placeholder="Имя"
               value={email}
+              onChange={handleChange}
               required={true}/>
           </label>
         </form>
-        <div className='profile__wrapper'>
-          <button className='profile__btn-submit'>
+        <div className="profile__wrapper">
+          <button
+            type="submit"
+            form="profile__form"
+            className="profile__btn-submit">
             Редактировать
           </button>
-          <button className='profile__btn-exit'>
+          <button
+            className="profile__btn-exit"
+            onClick={handleLogout}>
             Выйти из аккаунта
           </button>
         </div>
